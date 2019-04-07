3e4bcd82096b76f1ff1318ed4f349a743cb753c8
مهدی شکوری <09351266415@Aramooz.ir>
2019-04-07 18:20:45 +0430

    upload changes

---
+++ fornt/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// add your components here
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login'
+import signup from '@/components/signup'
+import addQuestion from '@/components/addQuestion'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      component: HelloWorld
+    },
+    {
+      path : '/login',
+      component: Login
+    },
+    {
+      path : '/signup',
+      component: signup
+    }
+    ,
+    {
+      path : '/addq',
+      component: addQuestion
+    }
+  ]
+})

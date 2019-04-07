8aa6595f61ab666c350e728bba10871f9fa4c3bd
مهدی شکوری <09351266415@Aramooz.ir>
2019-04-07 19:07:06 +0430

    route addQuestion added

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,7 @@ import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/login'
 import signup from '@/components/signup'
 import addExam from '@/components/addExam'
+import addQuestion from '@/components/addQuestion'
 
 Vue.use(Router)
 

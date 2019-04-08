9be6f0255880706b0a94d89487246bc8b2969254
Mahdi Shakouri <mahdi.shakourika@gmail.com>
2019-04-09 01:33:39 +0430

    doing: add question and choices :)

--- front/src/components/addQuestion.vue
+++ front/src/components/addQuestion.vue
@@ -1,8 +1,7 @@
 <template>
     <v-container>
-        <v-layout class="add-question-form">
-            <v-flex xs12 md2 lg3></v-flex>
-            <v-flex xs12 md8 lg6>
+        <v-layout class="add-question-form" justify-center>
+            <v-flex xs12 sm11 md10 lg8>
                 <v-card>
                     <v-toolbar class="light-blue lighten-1" >
                         <v-icon color="white">help</v-icon>
@@ -14,16 +13,15 @@
                                 <v-text-field style="margin-left:10px;" label="شماره سوال" required ></v-text-field>
                             </v-flex>
                             <v-flex xs12 md10>
-<v-text-field :counter="100" label="عنوان سوال" required></v-text-field>
+<v-text-field :counter="200" label="عنوان سوال" required></v-text-field>
                             </v-flex>
                         </v-layout>
                     </v-card-title>
                     <v-card-text>
-                        <choices :choicesList="choicesArray"></choices>
+                        <choices :choicesArray="emptyQuestion.choices" :editingProp="editing" @addOption="addOption2question" ></choices>
                     </v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex xs12 md2 lg3></v-flex>
         </v-layout>
     </v-container>
 </template>
@@ -33,16 +31,32 @@ import choices from '@/components/addQuestion/choices'
 export default {
 data(){
 return{
-    edit:null,
-    emptyChoices:{
+    editing:false,
+    emptyChoice:{
+            num:0,
             title:'',
             value:0
         },
     emptyQuestion:{
         title:'',
         rank:0,
+        choices:[{
+            num:0,
+            title:'1',
+            value:0
+        }]
     }    
 }
+},
+methods:{
+    addOption2question(){
+        if(this.editing){
+//
+        }else{
+            
+            this.emptyQuestion.choices.push(this.emptyChoice)
+        }
+    }
 },
 components:{
     choices
@@ -57,4 +71,18 @@ components:{
 .add-question-form .v-label{
     font-size:12px;
 }
+.add-question-form .add-question-choices>*{
+    height:70px;
+}
+.add-question-form .add-question-choices{ 
+    margin-bottom:5px;
+}
+.add-question-form .add-question-option{
+    height:inherit;
+}
+.add-question-form .add-question-option>*{
+    height:60px;
+    margin-top:5px;
+}
+
 </style>
--- front/src/components/addQuestion/choices.vue
+++ front/src/components/addQuestion/choices.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>
-        <v-list-tile v-for="(choice,n) in (choicesList.choices)" :key="'ch'+n">
+    <div v-if="editingProp">
+Mahdi
+    </div>
+    <div v-else >
+      <v-btn @click="addingOption()" flat class="success">
+        <v-icon>add_circle</v-icon>
+        افزودن گزینه</v-btn>
+        <v-list-tile v-for="(choice,n) in choicesArray" :key="'ch_'+n" class="add-question-choices">
               <v-list-tile-avatar>
-                <v-btn small depressed color="success" fab>
+
+                <v-btn small depressed color="info" fab>
                   {{n+1}}
                 </v-btn>
               </v-list-tile-avatar>
               <v-list-tile-content>
-                <v-list-tile-title :class=" (choicesList.dir=='rtl') ? 'text-xs-right': 'text-xs-left'" v-html="choice.title"></v-list-tile-title>
+                <!--<v-list-tile-title :class=" (choicesArray.dir=='rtl') ? 'text-xs-right': 'text-xs-left' >-->
+                <v-list-tile-title class="add-question-option" >
+                  <v-text-field :counter="100" label="پاسخ" required></v-text-field>
+                </v-list-tile-title>
               </v-list-tile-content>
         </v-list-tile>
     </div>
@@ -15,8 +25,20 @@
 
 <script>
 export default {
-porps:{
-    choicesList:Array
+  data(){
+    return{
+      //choicesArray:[],
+      //editingProp:false,
+    }
+  },
+props:{
+    choicesArray:Array,
+    editingProp:Boolean
+},
+methods:{
+  addingOption(){
+    this.$emit('addOption')
+  }
 }
 }
 </script>

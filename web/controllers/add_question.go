4db00d63e2642ba262ee3a5f0d6274141922ffca
مهدی شکوری <09351266415@Aramooz.ir>
2019-04-10 13:01:22 +0430

    doing: Create add question controller & data model

--- web/controllers/add_question.go
+++ web/controllers/add_question.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"Aramooz/services/response"
+
+	"github.com/kataras/iris"
+)
+
+type AddQuestion struct {
+}
+
+func (c *AddQuestion) Post(ctx iris.Context) response.Response {
+
+}

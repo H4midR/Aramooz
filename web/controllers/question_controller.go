b2bd5da8aa682c6323acf70b868dff9186102265
مهدی شکوری <09351266415@Aramooz.ir>
2019-04-10 16:12:34 +0430

    doing: AddQuestion Controller

--- web/controllers/question_controller.go
+++ web/controllers/question_controller.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	db "Aramooz/dataBaseServices"
 	"Aramooz/dataModels"
 	"Aramooz/services/response"
+	"encoding/json"
 
 	"github.com/kataras/iris"
 )
@@ -11,10 +13,23 @@ type AddQuestion struct {
 }
 
 func (c *AddQuestion) Post(ctx iris.Context) response.Response {
-	var quest dataModels.AddQuestion
+	var req dataModels.AddQuestion
 	var res response.Response
-	err := ctx.ReadJSON(&quest)
+	err := ctx.ReadJSON(&req)
 	res.HandleErr(err)
+
+	mgt := db.NewDgraphTrasn()
+	q, err := json.Marshal(req)
+	res.HandleErr(err)
+	if res.Code < 1 {
+		return res
+	}
+	_, Uids, err := mgt.Mutate(q)
+	res.HandleErr(err)
+	if res.Code < 1 {
+		return res
+	}
+	res.Data = Uids
 	return res
 
 }

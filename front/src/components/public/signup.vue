a48fb9b7d087b125265122dd79752f00ffa294f5
مریم پهلوان <09355975991@Aramooz.ir>
2019-04-11 11:20:19 +0430

    signup fixed

--- front/src/components/public/signup.vue
+++ front/src/components/public/signup.vue
@@ -1,40 +1,50 @@
 <template>
-    <v-container fluid full-height>
-        <v-layout  fluid justify-space-around align-center>
-          <v-flex xs12 md5>
-            {{response}}
-          </v-flex>
+    <v-container @keyup.enter="signup" fluid full-height>
+        <v-layout fluid  justify-space-around align-center>
             <v-flex xs12 md5>
                 <v-card xs12 md5>
                     <v-toolbar color="pink darken-3" dark>
                         فرم عضویت
                         <v-spacer></v-spacer>
                         <v-tooltip top>
-                        <v-btn color="secondary darken-3" slot="activator" flat icon router to="login" >
-                        <v-icon>mdi-login</v-icon>
+                        <v-btn color="secondary darken-3" slot="activator" flat icon  @click.native="$router.push('/login')">
+                            <v-icon>mdi-login</v-icon>
                         </v-btn>
                         <span>
-                        ورود
+                            ورود
                         </span>
                         </v-tooltip>
                     </v-toolbar>
 
+
                     <v-card-text>
                         <v-flex xs11>
-                            <v-text-field v-model="name" type="text" label="نام و نام خانوادگی"></v-text-field>
+                            <v-text-field :rules="[rules.required]" hint="نام خود را با حروف فارسی وارد نمایید" v-model="name" type="text" label="نام و نام خانوادگی"></v-text-field>
                         </v-flex>
                         <v-flex xs11>
-                            <v-text-field v-model="mobile" type="text" label="نام کاربری"></v-text-field>
+                            <v-text-field :rules="[rules.required, rules.minMobile]" hint="شماره موبایل را با اعداد انگلیسی وارد نمایید" v-model="mobile" type="text" mask="###########" label="موبایل"></v-text-field>
                         </v-flex>
                         <v-flex xs11>
-                            <v-text-field v-model="password" type="password" label="کلمه عبور"></v-text-field>
+                            <v-text-field :append-icon="showPass ? 'visibility' : 'visibility_off'" :rules="[rules.required, rules.minpass]" :type="showPass ? 'text' : 'password'"  @click:append="showPass = !showPass" v-model="password" label="کلمه عبور"></v-text-field>
                         </v-flex>
                     </v-card-text>
+                    <v-divider></v-divider>
                     <v-card-actions>
                         <v-tooltip top>
-                            <v-btn color="success" @click="signup" slot="activator">ثبت نام</v-btn>
+                            <v-btn block :loading="btnLoading" :disabled="btnLoading" color="success" @click="signup" slot="activator">
+                                ثبت نام
+                                <v-icon left>mdi-account-plus</v-icon>
+                            </v-btn>
+                            <span>ثبت نام در سایت</span>
                         </v-tooltip>
                     </v-card-actions>
+                
+                    <v-snackbar v-model="snackbar" color="success" :bottom="y === 'bottom'" :left="x === 'left'" :multi-line="mode === 'multi-line'" :right="x === 'right'" :timeout="timeout" :top="y === 'top'" :vertical="mode === 'vertical'" >
+                    {{ response }}
+                    <v-btn right flat fab @click="snackbar = false">
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                    </v-snackbar>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -48,29 +58,39 @@ export default {
         password:null,
         name:null,
         response:null,
+        btnLoading: false,
+        snackbar: false,
+        y: 'top',
+        x: 'right',
+        mode: '',
+        timeout: 4000,
+        showPass: false,
+        rules: {
+          required: value => !!value || 'اجباری!!',
+          minpass: v => v.length >= 8 || 'حداقل 8 عدد یا حرف',
+          minMobile: v => v.length >= 11 || 'شماره موبایل باید 11 عدد باشد!',
+        }
     }},
     methods:{
         signup(){
+            this.btnLoading = true
             /*
             required
             name
             mobile
             password
             */
+           
             this.axios.post("http://localhost:9090/user",JSON.stringify({
                 mobile:this.mobile,
                 password:this.password,
                 name:this.name,
             })).then(res=>{
               this.response=res.data.Message
+              this.snackbar = true
+              this.btnLoading = false
             })
-
         }
     }
 }
 </script>
- <style  scoped>
-
-</style>
-
-
